870c67155b3d70d32c6abff24ba1267f1700382c
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-18 10:23:30 -0300

    Passing test - POST /users

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error(`User with id ${user_id} could not be found`);
+    }
+
+    this.usersRepository.turnAdmin(user);
+
+    return user;
   }
 }
 
