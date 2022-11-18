465c8970b2a53be28ca77c4202317ecb7d419b6a
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-18 09:18:04 -0300

    Implementing UsersRepository methods

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,14 +1,19 @@
 import { v4 as uuidV4 } from "uuid";
 
+import { ICreateUserDTO } from "../repositories/IUsersRepository";
+
 export class User {
   id: string;
   name: string;
   admin: boolean;
   email: string;
   created_at: Date;
-  updated_at: Date;
+  updated_at?: Date;
 
-  constructor() {
+  constructor({ name, email }: ICreateUserDTO) {
+    this.name = name;
+    this.email = email;
+
     if (!this.id) {
       this.id = uuidV4();
       this.admin = false;
