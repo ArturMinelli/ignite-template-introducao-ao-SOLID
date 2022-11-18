917d7c847b3acb2270370e14e09d9fe344ccf1ae
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-18 10:06:58 -0300

    Passing listAllUsers test

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error(`User with id ${user_id} could not be found`);
+    }
+
+    if (!user.admin) {
+      throw new Error(
+        `User with id ${user_id} is not authorized to list users`
+      );
+    }
+
+    return this.usersRepository.list();
   }
 }
 
